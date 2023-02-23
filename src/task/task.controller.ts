6c08480ebdd0bce30deb5451ab78e2bd300fc5ea
MoisoDavid <wollxi@gmail.com>
2023-02-23 14:55:29 -0500

    TasksWithErrorHandler

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -3,6 +3,7 @@ import { Task } from 'src/types/task';
 import { theTaskDto } from 'src/types/dto/task.dto';
 import { TaskService } from './task.service';
 import { updateTaskDto } from 'src/types/dto/updateTask.dto';
+import { errorMessage } from 'src/types/errorMessage';
 
 @Controller('task')
 export class TaskController {
@@ -12,31 +13,31 @@ export class TaskController {
 
     /* Route to get all tasks availables no matter user */
     @Get()
-    getTasks(): Promise<Task[]> {
+    getTasks(): Promise<Task[] | errorMessage > {
         return this.taskService.getAll()
     }
 
     /* Route to get specific task searched by id */
     @Get(':id')
-    getTask(@Param('id') string: string): Promise<Task> {
+    getTask(@Param('id') string: string): Promise<Task | errorMessage> {
         return this.taskService.getById(string);
     }
 
     /* Route to create and save a new task, receives a task by parameter */
     @Post()
-    postTask(@Body() task: theTaskDto): Promise<Task>{
+    postTask(@Body() task: theTaskDto): Promise<Task | errorMessage>{
         return this.taskService.createTask(task)
     }
 
     /* Rout to update an existing task, we receive an object which have an id and the task info to update */
     @Put()
-    editTask(@Body() updateTask: updateTaskDto): Promise<Task>{
+    editTask(@Body() updateTask: updateTaskDto): Promise<Task | errorMessage>{
         return this.taskService.updateTask(updateTask.id, updateTask.task)
     }
 
-    /* Route to delete a task by id */
+    /* Route to delete a task by id */       /* errorMessage can work just as message too */
     @Delete(':id')
-    deleteTask(@Param('id') string: string): Promise<string>{
+    deleteTask(@Param('id') string: string): Promise<errorMessage>{
         return this.taskService.deleteTask(string)
     }
 }
